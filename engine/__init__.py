0e1bb2b449c788b18eba0b7f47bdd90694118d69
SamChu69 <fptg1234@gmail.com>
2025-12-13 03:02:29 +0800

    Create __init__.py

---
+++ engine/__init__.py
@@ -0,0 +1,10 @@
+"""
+TAITS - Taiwan Alpha Intelligence Trading System
+engine package
+
+This package contains core runtime engines:
+- orchestrator
+- audit_trail
+- data_validator
+- risk_engine
+"""
